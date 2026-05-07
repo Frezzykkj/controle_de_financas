c42a545f3d25da8e3da36789a7bcead7b37e010e
João Dantas <joaorocha130709@gmail.com>
2026-05-07 01:29:58 -0300

    correção de bugs

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,7 +1,19 @@
 from repositories.usuario_repo import cadastro_usuario, buscar_usuario
 from utils.security import verificar_senha
+import re
+
+def validar_email(email):
+    padrao = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    
+    # re.match verifica se o e-mail segue o padrão
+    if re.match(padrao, email):
+        return True
+    return False
 
 def registrar(usuario, email, senha):
+    if not validar_email(email):
+        raise Exception("O formato do e-mail não é válido. Use algo como nome@exemplo.com")
+    
     cadastro_usuario(usuario, email, senha)
 
 def login(email, senha):
